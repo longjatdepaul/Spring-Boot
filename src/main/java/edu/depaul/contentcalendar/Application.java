31c4c08832e24dd396686aca78a864432f366d1b
Lee Long <longj@depaulcatholic.org>
2023-10-25 23:56:34 -0400

    adding data persistency with H2 DB exports

--- src/main/java/edu/depaul/contentcalendar/Application.java
+++ src/main/java/edu/depaul/contentcalendar/Application.java
@@ -1,37 +1,12 @@
 package edu.depaul.contentcalendar;
 
-import edu.depaul.contentcalendar.model.Content;
-import edu.depaul.contentcalendar.model.Status;
-import edu.depaul.contentcalendar.model.Type;
-import edu.depaul.contentcalendar.repository.ContentRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean
-	CommandLineRunner commandLineRunner(ContentRepository repository) {
-		return args -> {
-			// insert some data into the database
-			Content content = new Content(
-					null,
-					"Hello Chat GPT",
-					"All about Chat GPT",
-					Status.IDEA,
-					Type.VIDEO,
-					LocalDateTime.now(),
-					null,
-					"");
-			repository.save(content);
-		};
 	}
 }
--- src/main/java/edu/depaul/contentcalendar/config/DataLoader.java
+++ src/main/java/edu/depaul/contentcalendar/config/DataLoader.java
@@ -1,12 +1,46 @@
 package edu.depaul.contentcalendar.config;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Controller;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.h2.tools.RunScript;
+import org.h2.tools.Script;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-//@Controller
-public class DataLoader implements CommandLineRunner {
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("Hello World");
+import javax.sql.DataSource;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Component
+public class DataLoader {
+    private final DataSource dataSource;
+    private final String h2dbFile;
+
+    public DataLoader(DataSource dataSource,
+                      @Value("${content.h2db.file}") String h2dbFile) {
+        this.dataSource = dataSource;
+        this.h2dbFile = h2dbFile;
+    }
+
+    @PostConstruct
+    private void postConstruct() {
+        // load the database from an H2 DB export file
+        try (Connection conn = dataSource.getConnection()) {
+            RunScript.execute(conn, new FileReader(h2dbFile));
+        } catch (SQLException | FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        // dump the database to an H2 DB export file
+        try (Connection conn = dataSource.getConnection()) {
+            Script.process(conn, h2dbFile, "", "");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
