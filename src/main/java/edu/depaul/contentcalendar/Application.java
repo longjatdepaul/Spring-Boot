5ad82b705de3473cd81975dc4ff227c83feb4ddb
Lee Long <longj@depaulcatholic.org>
2023-11-26 13:31:42 -0500

    adding application configuration

--- src/main/java/edu/depaul/contentcalendar/Application.java
+++ src/main/java/edu/depaul/contentcalendar/Application.java
@@ -1,8 +1,11 @@
 package edu.depaul.contentcalendar;
 
+import edu.depaul.contentcalendar.config.ContentCalendarProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(ContentCalendarProperties.class)
 @SpringBootApplication
 public class Application {
 
